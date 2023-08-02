e27813ac1314e54e2af89f66f7fef486972db19a
anhng <anananh1999@gmail.com>
2023-08-02 18:44:42 +1000

    Create post request with validation pipe

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Param, ParseIntPipe, Post, UsePipes, ValidationPipe } from "@nestjs/common";
+import { UserDto } from "./user.dto";
+import { ValidationTypes } from "class-validator";
 
 @Controller("users")
 export class UsersController {
@@ -14,5 +16,17 @@ export class UsersController {
                 age: 29
             }
         ]
+    };
+    @UsePipes(new ValidationPipe())
+    @Post()
+    createUser(@Body() user: UserDto):UserDto  {
+        return {
+            username: "anh",
+            password: "test"
+        }
+    };
+    @Get(":id")
+    getUserById(@Param("id",ParseIntPipe) id: number)  {
+        return id;
     }
 }
