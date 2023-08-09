65a49a1ff62cb5cee19cfd8877bd8e999d442210
anhng <anananh1999@gmail.com>
2023-08-09 21:43:17 +1000

    modify storemodule with forRoot and forModule

--- store/store.module.ts
+++ store/store.module.ts
@@ -1,12 +1,23 @@
 import { DynamicModule, Module } from "@nestjs/common";
 import { StoreService } from "./store.service";
-export interface StoreConfig {
-    dirName: string;
-    fileName: string;
-}
+import { StoreConfig, StoreFeatureConfig, StoreRootConfig } from "./store.config";
+let rootStoreConfig: StoreConfig;
+const DEFAULT_DIR_NAME ="store";
+const DEFAULT_FILE_NAME = "data.json";
 @Module({})
 export class StoreModule {
-    static register(config: StoreConfig): DynamicModule {
+    static forRoot(config?: StoreRootConfig): DynamicModule {
+        return {
+            // name of module
+            module: StoreModule,
+            providers: [StoreService, {
+                provide: "STORE_CONFIG",
+                useValue: config
+            }],
+            exports: [StoreService]
+        };
+    };
+    static forFeature(config: StoreFeatureConfig): DynamicModule {
         return {
             // name of module
             module: StoreModule,
@@ -17,4 +28,5 @@ export class StoreModule {
             exports: [StoreService]
         };
     }
+
 }
