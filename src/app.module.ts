bd62dc1c5ce3b3923ec316adcfc7bd00862f021d
anhng <anananh1999@gmail.com>
2023-08-16 11:21:06 +1000

    Install orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,19 @@ import { UsersModule } from './users/users.module';
 import { CatModule } from './cat/cat.module';
 import { PostsModule } from './posts/posts.module';
 import { StoreModule } from 'store/store.module';
-
+import {TypeOrmModule} from '@nestjs/typeorm';
 @Module({
-  imports: [UsersModule, CatModule, PostsModule, StoreModule.forRoot()],
+  imports: [UsersModule, CatModule, PostsModule, StoreModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
